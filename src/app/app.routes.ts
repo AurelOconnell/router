ed33f923f91e8ac17dccde6cffc450adc6d47e06
AurelOconnell <aurelienoconnell@gmail.com>
2023-09-21 09:56:40 +0200

    finished quest router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
-import { MenuComponent } from "./menu/menu.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'contact', component: ContactComponent},
-    { path: 'menu', component: MenuComponent},
+    { path: 'sign-up', component: SignUpComponent},
+    { path: 'user-profile', component: UserProfileComponent},
+    { path: '', component: UserProfileComponent},
 ];
 
 export { ROUTES };
